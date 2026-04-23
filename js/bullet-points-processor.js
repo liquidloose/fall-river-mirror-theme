5c52a5b51ed102bc0e45a94051e3fdf49ded0bf6
liquidloose <ronaldlussier83@gmail.com>
2026-04-23 23:47:09 +0000

    Added bullet-points-processor.js

---
+++ js/bullet-points-processor.js
@@ -0,0 +1,7 @@
+/**
+ * Frontend hook for bullet-point content (Post ID block outputs HTML server-side).
+ * Reserved for future enhancements; keeps the enqueue target from 404ing.
+ */
+(function () {
+	'use strict';
+})();
